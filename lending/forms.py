2233b7c32e123c1dc653b4fa80649ce827b50950
AndyShafer <andyshafer0715@gmail.com>
2018-09-21 11:27:22 -0400

    Fixed login form

--- lending/forms.py
+++ lending/forms.py
@@ -9,9 +9,7 @@ class UserForm(forms.ModelForm):
         #fields = ['firstName', 'lastName', 'username', 'email', 'phone', 'address', 'city', 'state', 'password']
         fields = ['username', 'email', 'password']
 
-class LoginForm(forms.ModelForm):
+class LoginForm(forms.Form):
+    username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
 
-    class Meta:
-        model = User
-        fields = ['username', 'password']

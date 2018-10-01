0f7e217df86ccf3c1344a643ebdf6f69075deb9e
AndyShafer <andyshafer0715@gmail.com>
2018-09-30 23:39:32 -0400

    Update profile view

--- lending/views.py
+++ lending/views.py
@@ -3,8 +3,10 @@ from django.http import HttpResponse, Http404
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login
 from django.views import generic
+from django.views.generic.edit import UpdateView
 from django.views.generic import View
-#from .models import User
+from django.urls import reverse_lazy
+from .models import Profile
 from .forms import UserForm, LoginForm
 
 
@@ -41,13 +43,31 @@ class UserFormView(View):
             user.save()
 
             user = authenticate(username=username, password=password)
+            
+            profile = Profile.objects.create(user=user)
 
             if user is not None:
                 if user.is_active:
                     login(request, user)
-                    return redirect('lending:dashboard')
+                    return redirect('lending:profile-edit', pk=profile.id)
 
         return render(request, self.template_name, {'form': form})
+
+class UpdateProfile(UpdateView):
+    model = Profile
+    fields = ['address', 'city', 'state', 'bio']
+    success_url = reverse_lazy('lending:dashboard')
+
+    def user_passes_test(self, request):
+        if request.user.is_authenticated:
+            self.object = self.get_object()
+            return self.object.user == request.user
+        return False
+
+    def dispatch(self, request, *args, **kwargs):
+        if not self.user_passes_test(request):
+            return redirect('lending:login')
+        return super(UpdateProfile, self).dispatch(request, *args, **kwargs)
 
 class LoginFormView(View):
     form_class = LoginForm
